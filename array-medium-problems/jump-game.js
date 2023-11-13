a5b9ddb3a345f5bbb1abb0e224d00de7e7243352
osamaazhar <osamaazhar.edu@gmail.com>
2023-11-13 23:01:42 +0500

    jump case in progress

---
+++ array-medium-problems/jump-game.js
@@ -0,0 +1,26 @@
+////////////////////////////////////////////INITIAL APPROACH://////////////////////////////////////////////////////////////////////////
+// //130 tescases passing of 172
+// var canJump = function (nums) {
+//     if (nums.length === 1) return true
+//     if (nums.length === 2 && nums[1] === 0) return true
+//     // if(nums[nums.length-1] === 0) return true
+//     nums.sort((a, b) => a - b)
+//     if (nums[0] === 0) return false;
+//     else return true
+// };
+
+// canJump([2, 3, 1, 1, 4])
+// canJump([3,2,1,0,4])
+// canJump([2, 0])
+
+
+////////////////////////////////////////////INITIAL APPROACH://////////////////////////////////////////////////////////////////////////
+
+var canJump = function (nums) {
+}
+
+// canJump([2, 3, 1, 1, 4])
+// canJump([3, 2, 1, 0, 4])
+// canJump([2, 0])
+// canJump([2,0,0])
+
