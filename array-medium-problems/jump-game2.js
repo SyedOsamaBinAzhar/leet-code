d727f865e76844f81a2e44d683f32b4ae4c1d387
osamaazhar <osamaazhar.edu@gmail.com>
2023-12-02 07:28:47 +0500

    jump-case2 solved

--- array-medium-problems/jump-game2.js
+++ array-medium-problems/jump-game2.js
@@ -1,29 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var jump = function (nums) {
-    // let finalPosition = nums.length;
-    let finalPosition = nums.length - 1;
 
-    let count = 0;
+    let coverage = 0;
+    let destination = nums.length - 1;
+    let lastJumpIndex = 0;
+    let totalJumps = 0;
+
+    if (nums.length === 1) return 0;
+
     for (let i = 0; i < nums.length; i++) {
-        if (i + nums[i] >= finalPosition) {
-            console.log("true1")
-            count++;
-            break;
-        } else if ((i + nums[i]) <= nums[i + 1]) {
-            console.log("true2")
-            count++
-        }
-        else if ((i + nums[i]) > nums[i + 1]) {
-            console.log("true3")
-            i = i + nums[i]
-            count++
+        coverage = Math.max(coverage, i + nums[i]);
+
+        if (i === lastJumpIndex) {
+            lastJumpIndex = coverage;
+            totalJumps++;
+
+            if (coverage >= destination) {
+                return totalJumps
+            }
         }
     }
 
-    // return count
-    console.log("count", count)
+    return totalJumps
 }
 
-// jump([2, 3, 1, 1, 4])
-jump([2, 3, 0, 1, 4])
-// jump([0])
-// jump([1, 1, 2, 5, 2, 1, 0, 0, 1, 3])
