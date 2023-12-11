9594f7ba34eaf183f6f957590e4dc363da8bfdb6
osamaazhar <osamaazhar.edu@gmail.com>
2023-12-11 22:36:49 +0500

    selection sort written by me

--- sorting-algorithms/selection-sort.js
+++ sorting-algorithms/selection-sort.js
@@ -1,5 +1,24 @@
-function selectionSort(){
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minValIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if ((j <= arr.length - 1) && (arr[j] < arr[minValIndex])) {
+                minValIndex = j
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[minValIndex];
+        arr[minValIndex] = temp
+
+    }
+    return arr
+    // console.log("arr", arr)
 
 }
 
-selectionSort();
+
+console.log(selectionSort([5, 3, 4, 1, 2]))
+// selectionSort([5, 3, 4, 1, 2])
+// selectionSort([5, 4, 3, 2, 1])
+// selectionSort([19, 44, 38, 5, 47, 15])
+
